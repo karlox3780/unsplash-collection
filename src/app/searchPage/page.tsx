af10829f9372211f2a2b623d9f7b72caa99fa27c
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-06-06 22:21:06 +0200

    Cambios en el diseño al ejecutar la búsqueda.

--- src/app/searchPage/page.tsx
+++ src/app/searchPage/page.tsx
@@ -8,6 +8,7 @@ import ListImages from "../../components/ListImages";
 export default function Page() {
     const [searchImageState, setSearchImageState] = useState("");
     const [gallery, setGallery] = useState([]);
+    const [search, setSearch] = useState(false);
     const handleSubmit = (e: any) => {
         e.preventDefault();
         PhotosService
@@ -17,18 +18,20 @@ export default function Page() {
                 setGallery(res.results);
             })
             .catch((err) => console.log(err))
-            .finally(() => { });
+            .finally(() => { setSearch(true); });
     }
     const handleChange = (e: any) => {
         setSearchImageState(e.target.value);
     }
     return (
-        <div className="searchPage">
+        <div className={`${search ? "!bg-none " : ""}searchPage overflow-x-hidden`}>
             <div className="searchPage_title flex flex-col items-center mt-[240px]">
                 <Image className="searchPage_background_left_mobile" src='/images/hero-left.png' alt="Mobile background left image" height={797} width={537} />
                 <Image className="searchPage_background_right_mobile" src='/images/hero-right.png' alt="Mobile background right image" height={797} width={537} />
-                <h1 className="text-[#121826] text-[36px] font-semibold">Search</h1>
-                <h2 className="text-[#121826] text-[16px] font-medium mt-[8px]">Search high-resolution images from Unsplash</h2>
+                <div className={`${search ? "hidden " : ""}text-center`}>
+                    <h1 className="text-[#121826] text-[36px] font-semibold">Search</h1>
+                    <h2 className="text-[#121826] text-[16px] font-medium mt-[8px]">Search high-resolution images from Unsplash</h2>
+                </div>
                 <form onSubmit={handleSubmit}>
                     <input className="searchPage_input text-[#E5E7EB] text-[16px] font-medium" type="text" name="name" placeholder="Enter your keywords..." onChange={handleChange} />
                 </form>
@@ -36,6 +39,6 @@ export default function Page() {
                     gallery.length > 0 && <ListImages gallery={gallery} />
                 }
             </div>
-        </div>
+        </div >
     );
 }
