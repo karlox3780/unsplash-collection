b42024e20d21696388842b014e7a1d1ebf1569eb
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-06-08 23:53:56 +0200

    Cambios en el diseño de la búsqueda de imagenes.

--- src/app/searchPage/page.tsx
+++ src/app/searchPage/page.tsx
@@ -11,6 +11,7 @@ export default function Page() {
     const [search, setSearch] = useState(false);
     const handleSubmit = (e: any) => {
         e.preventDefault();
+        e.stopPropagation();
         PhotosService
             .getPhotos(searchImageState)
             .then((res) => {
@@ -26,15 +27,15 @@ export default function Page() {
     return (
         <div className={`${search ? "!bg-none " : ""}searchPage overflow-x-hidden`}>
             <div className={`searchPage_title flex flex-col items-center ${search ? "mt-[50px] " : "mt-[240px]"}`}>
-                <div className="searchPage_imageTop absolute top-0 w-full h-[100px]"></div>
+                <div className={`searchPage_imageTop absolute top-0 w-full h-[100px] z-[-1] ${search ? "block " : "hidden"}`}></div>
                 <Image className="searchPage_background_left_mobile" src='/images/hero-left.png' alt="Mobile background left image" height={797} width={537} />
                 <Image className="searchPage_background_right_mobile" src='/images/hero-right.png' alt="Mobile background right image" height={797} width={537} />
                 <div className={`${search ? "hidden " : ""}text-center`}>
                     <h1 className="text-[#121826] text-[36px] font-semibold">Search</h1>
                     <h2 className="text-[#121826] text-[16px] font-medium mt-[8px]">Search high-resolution images from Unsplash</h2>
                 </div>
-                <form onSubmit={handleSubmit}>
-                    <input className="searchPage_input text-[#E5E7EB] text-[16px] font-medium" type="text" name="name" placeholder="Enter your keywords..." onChange={handleChange} />
+                <form className="mb-[48px]" onSubmit={handleSubmit}>
+                    <input className="searchPage_input text-[#E5E7EB] text-[16px] font-medium bg-[#FFFFFF] focus:bg-[#FFFFFF]" type="text" name="name" placeholder="Enter your keywords..." onChange={handleChange} />
                 </form>
                 {
                     gallery.length > 0 && <ListImages gallery={gallery} />
