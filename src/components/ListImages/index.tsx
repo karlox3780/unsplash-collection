687000b333a187f098b152d4fe8a481d9daf9ded
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-06-09 03:35:24 +0200

    Diseño de la galeria.

--- src/components/ListImages/index.tsx
+++ src/components/ListImages/index.tsx
@@ -4,10 +4,10 @@ type Props = {
 }
 export default function ListImages({ gallery }: Props) {
     return (
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-[24px] w-[90vw] mb-[48px]">
             {gallery.map((image: any) => (
                 <div key={image.id}>
-                    <Image className="h-auto max-w-full rounded-lg" src={image.urls.full} alt={image.alt_description} width={80} height={80} />
+                    <Image className="h-auto max-w-full rounded-lg w-[100%]" src={image.urls.full} alt={image.alt_description} width={500} height={500} />
                 </div>))
             }
         </div>
