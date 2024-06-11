e8defdfa1d6054aee43d7f36102a41750962ebb1
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-06-11 23:13:03 +0200

    Nueva distribución del layout de la galeria de imagenes.

--- src/components/ListImages/index.tsx
+++ src/components/ListImages/index.tsx
@@ -3,12 +3,17 @@ type Props = {
     gallery: any
 }
 export default function ListImages({ gallery }: Props) {
+
     return (
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-[24px] w-[90vw] mb-[48px]">
-            {gallery.map((image: any) => (
-                <div key={image.id}>
-                    <Image className="h-auto max-w-full rounded-lg w-[100%]" src={image.urls.full} alt={image.alt_description} width={500} height={500} />
-                </div>))
+        <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-[24px] w-[90vw] mb-[48px]">
+            {
+                gallery.map((image: any, index: any) => {
+                    return (
+                        <div key={image.id} className="mb-[24px] break-inside-avoid">
+                            <Image className="h-auto max-w-full rounded-lg w-[100%]" src={image.urls.full} alt={image.alt_description} width={500} height={500} />
+                        </div>
+                    )
+                })
             }
         </div>
     )
