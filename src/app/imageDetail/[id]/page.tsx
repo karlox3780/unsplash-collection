09d0c034abe309256afdabec2720a207ac166689
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-06-19 23:35:48 +0200

    Cambio en la imagen del detalle.

--- src/app/imageDetail/[id]/page.tsx
+++ src/app/imageDetail/[id]/page.tsx
@@ -22,9 +22,14 @@ export default function Page() {
 
     return (
         photo !== null && <div className="imageDetail_container flex items-start justify-center py-[52px] mx-[60px]">
-            <Image className="rounded w-[50%] max-h-full" src={photo.urls.full} alt="Mobile background left image" height={500} width={500} />
-            <div className='w-[50%]'>
-                <Image className="rounded-full" src={photo.user.profile_image.medium} height={50} width={50} alt="" /><span>{photo.user.name}</span>
+            <div className='w-[50%] h-[100%] flex justify-end'>
+                <Image className="rounded h-auto w-auto max-h-full" src={photo.urls.full} alt="Mobile background left image" height={500} width={500} />
+            </div>
+            <div className='w-[50%] ml-[30px]'>
+                <div className='flex items-center'>
+                    <Image className="rounded-full" src={photo.user.profile_image.medium} height={40} width={40} alt={photo.user.name} />
+                    <label className='ml-[10px] text-[#121826]'>{photo.user.name}</label>
+                </div>
             </div>
         </div>
     )
