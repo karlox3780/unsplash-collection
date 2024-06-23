b1334445c97dbd7e3ea5bbc095887bc91de8334e
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-06-23 22:12:38 +0200

    Cambio en el diseño del focus del boton.

--- src/app/imageDetail/[id]/page.tsx
+++ src/app/imageDetail/[id]/page.tsx
@@ -36,9 +36,9 @@ export default function Page() {
                     <label className='text-[#121826] text-[12px] font-medium'>{`Published on ${moment(photo.created_at).format('LL')}`}</label>
                 </div>
                 <div className='mt-[15px]'>
-                    <button type="button" className="text-[#121826] text-[14px] bg-[#E5E7EB] hover:bg-[#E5E7EB] focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 me-2 mb-2">Add to Collection</button>
+                    <button type="button" className="text-[#121826] text-[14px] bg-[#E5E7EB] hover:bg-[#E5E7EB] font-medium rounded otext-sm px-5 py-2.5 mb-2 mr-[20px]">Add to Collection</button>
                     <Link href={`${photo.links.download}&force=true`} rel='nofollow' download target='_blank'>
-                        <button type="button" className="text-[#121826] text-[14px] bg-[#E5E7EB] hover:bg-[#E5E7EB] focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 me-2 mb-2">
+                        <button type="button" className="text-[#121826] text-[14px] bg-[#E5E7EB] hover:bg-[#E5E7EB] font-medium rounded text-sm px-5 py-2.5 me-2 mb-2">
                             Download</button>
                     </Link>
                 </div>
