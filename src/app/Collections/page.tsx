77d4bb1d15a040c3336d6839d53ff1083d4bda89
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-06-30 20:55:56 +0200

    Cambios en la página collections.

--- src/app/Collections/page.tsx
+++ src/app/Collections/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import './page.css';
 import { useEffect, useState } from "react";
 import UsersService from "@/services/Users.service";
 
@@ -16,7 +17,10 @@ export default function Page() {
 
     return (
         <div className="collections_container">
-
+            <div className="flex flex-col items-center mt-[36px]">
+                <h1 className="collections_title w-fit text-[36px] font-semibold bg-[size:100%_100%] bg-center">Collections</h1>
+                <p className='text-[#121826] text-center mt-[8px] text-[16px] font-medium'>Explore the world through collections of beautiful <br />photos free to use under the <u>Unsplash License</u>.</p>
+            </div>
         </div>
     );
 }
